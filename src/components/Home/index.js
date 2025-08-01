71a89786933d27734f6bf61015bcda58b2753cf1
Panasa Satya Sai <satyasaipanasa@gmail.com>
2025-08-01 12:34:02 +0530

    Chainged

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,13 @@
 import {Component} from 'react'
-
+import Header from '../Header'
 class Home extends Component{
     render(){
         return(
             <div>
-                <h1>Home Component</h1>
+                <Header/>
+                <h1>Book Store</h1>
+                <p>Description</p>
+                <button>Explore Books</button>
             </div>
         )
     }
